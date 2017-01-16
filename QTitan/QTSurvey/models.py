375043adde114f7f2e694c093fdf179290e771c5
rdespoiu <roberto.despoiu@gmail.com>
2017-01-16 12:33:32 -0600

    New db schema

--- QTitan/QTSurvey/models.py
+++ QTitan/QTSurvey/models.py
@@ -5,45 +5,49 @@ from django.db import models
 # DB Models #
 #############
 
-# Researchers
+# Users
+# RSFlag designates researcher. False by default, so when users register they will automatically be subjects. Researchers are manually added
 class User(models.Model):
+    RSFlag = models.BooleanField(default = False)
     email = models.CharField(max_length = 80)
-    firstName = models.CharField(max_length = 40)
-    lastName = models.CharField(max_length = 40)
-    phone = models.CharField(max_length = 11)
     password = models.CharField(max_length = 128)
-    salt = models.CharField(max_length = 32)
+    salt = models.CharField(max_length = 128)
 
-# Subjects
-class Subject(models.Model):
-    researcherID = models.ForeignKey(User, on_delete = models.CASCADE)
+# Demographics common to all users (researchers/subjects)
+class BaseDemographic(models.Model):
+    userID = models.ForeignKey(User, on_delete = models.CASCADE)
     firstName = models.CharField(max_length = 40)
     lastName = models.CharField(max_length = 40)
-    email = models.CharField(max_length = 80)
     phone = models.CharField(max_length = 11)
+    dob = models.DateField()
 
-# Additional subject demographic information if researchers need it
+# Custom demographics that some researchers may require subjects to populate
 class CustomDemographic(models.Model):
-    subjectID = models.ForeignKey(Subject, on_delete = models.CASCADE)
-    title = models.CharField(max_length = 60)
-    description = models.CharField(max_length = 60)
+    userID = models.ForeignKey(User, on_delete = models.CASCADE)
+    field = models.CharField(max_length = 40)
+    value = models.CharField(max_length = 40)
 
-# Surveys
+# Survey
+# Distribution indicates whether a survey is open to all or by invite only. Default False = invite only
 class Survey(models.Model):
     ownerID = models.ForeignKey(User, on_delete = models.CASCADE)
-    title = models.CharField(max_length = 200)
-    description = models.CharField(max_length = 500)
+    title = models.CharField(max_length = 256)
+    description = models.CharField(max_length = 512)
     distribution = models.BooleanField(default = False)
 
-# Questions in surveys
-class Question(models.Model):
+# Survey option
+class SurveyField(models.Model):
     surveyID = models.ForeignKey(Survey, on_delete = models.CASCADE)
-    title = models.CharField(max_length = 200)
-    description = models.CharField(max_length = 500)
+    value = models.CharField(max_length = 256)
 
-# Subject responses to surveys
-class SurveyResponse(models.Model):
-    questionID = models.ForeignKey(Question, on_delete = models.CASCADE)
+# Completed surveys. orderPosition designates in which order each SurveyField was placed
+class CompletedSurvey(models.Model):
     surveyID = models.ForeignKey(Survey, on_delete = models.CASCADE)
-    subjectID = models.ForeignKey(Subject, on_delete = models.CASCADE)
-    respone = models.CharField(max_length = 500)
+    surveyFieldID = models.ForeignKey(SurveyField, on_delete = models.CASCADE)
+    userID = models.ForeignKey(User, on_delete = models.CASCADE)
+    orderPosition = models.IntegerField()
+
+# For survey distribution, to check whether a subject has access to a survey.
+class SurveyAccess(models.Model):
+    surveyID = models.ForeignKey(Survey, on_delete = models.CASCADE)
+    userID = models.ForeignKey(User, on_delete = models.CASCADE)
