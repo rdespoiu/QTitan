40568171417776c1001523911bfa44bc40024257
Roberto Despoiu <robertodespoiu@Narcisas-MacBook-Air.local>
2017-01-20 10:26:04 -0600

    Registered DB models with administrative module (also added a .gitignore file)

--- QTitan/QTSurvey/admin.py
+++ QTitan/QTSurvey/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import \
+                    User,               \
+                    BaseDemographic,    \
+                    CustomDemographic,  \
+                    Survey,             \
+                    SurveyField,        \
+                    CompletedSurvey,    \
+                    SurveyAccess
 
-# Register your models here.
+# Model registration
+admin.site.register(User)
+admin.site.register(BaseDemographic)
+admin.site.register(CustomDemographic)
+admin.site.register(Survey)
+admin.site.register(SurveyField)
+admin.site.register(CompletedSurvey)
+admin.site.register(SurveyAccess)
