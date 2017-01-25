21936fa4cf2d8c2c6f46cdc7e11b6e49862c691c
rdespoiu <roberto.despoiu@gmail.com>
2017-01-25 10:27:55 -0600

    App template complete, ready for new modules and authentication

--- QTitan/QTSurvey/views.py
+++ QTitan/QTSurvey/views.py
@@ -7,45 +7,26 @@ from django.contrib.auth import (authenticate, get_user_model, login, logout,)
 from .forms import UserLoginForm
 from django.shortcuts import redirect
 
-class TestUser:
-    firstname = 'FName'
-    lastname = 'LName'
-    email = 'fnamelname@email.com'
-    rsflag = True
-
-'''
-def checkForSession()
-This method can be used to check whether a user is currently logged in, and redirect appropriately if attempting to access unauthorized views (such as login/register if logged in, or others if not logged in)
-Implement this
-'''
-
-def testContext():
-    loggedInUser = TestUser()
-    return {'loggedInUser': loggedInUser}
 
 # Views
 
 # Index only redirects. If there is no user session, redirect to login page.
 # If there IS a session, redirect to 'surveys'
 def index(request):
-    context = testContext()
+    context = {}
 
-    if not context['loggedInUser']:
+    if not context.get('loggedInUser'):
         return redirect('login')
     else:
         return redirect('surveys')
 
 # Registration
 def register(request):
-    loggedInUser = TestUser()
-    loggedInUser = None
-
     template = loader.get_template('QTSurvey/register.html')
     return HttpResponse(template.render(request))
 
 def login(request):
     template = loader.get_template('QTSurvey/login.html')
-
     form = UserLoginForm(request.POST or None)
 
     print('\n\nform valid: {}\n\n'.format(form.is_valid()))
@@ -69,7 +50,7 @@ def login(request):
     context = {'form': form}
 
     return HttpResponse(template.render(context, request))
-    
+
 
 def logout_view (request):
     return render(request, 'form.html',{})
@@ -78,52 +59,32 @@ def logout_view (request):
 
 
 
-
-
+# delete this import
+import random
 def surveys(request):
-    context = testContext()
-
-    if not context['loggedInUser']: return redirect('login')
-
-    if context['loggedInUser'].rsflag:
+    if random.randint(0,1) == 1:
         template = loader.get_template('QTSurvey/researcher-surveys.html')
     else:
         template = loader.get_template('QTSurvey/subject-available-surveys.html')
 
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(request))
 
 def researcher_analytics(request):
-    context = testContext()
-
-    if not context['loggedInUser']: return redirect('login')
-
     template = loader.get_template('QTSurvey/researcher-analytics.html')
 
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(request))
 
 def researcher_subjects(request):
-    context = testContext()
-
-    if not context['loggedInUser']: return redirect('login')
-
     template = loader.get_template('QTSurvey/researcher-subjects.html')
 
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(request))
 
 def subject_completed_surveys(request):
-    context = testContext()
-
-    if not context['loggedInUser']: return redirect('login')
-
     template = loader.get_template('QTSurvey/subject-completed-surveys.html')
 
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(request))
 
 def create_survey(request):
-    context = testContext()
-
-    if not context['loggedInUser']: return redirect('login')
-
     template = loader.get_template('QTSurvey/create-survey.html')
 
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(request))
