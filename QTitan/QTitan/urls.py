245f370ce0d51c49109459e74b6a49eb96d26738
rdespoiu <roberto.despoiu@gmail.com>
2017-02-20 11:04:47 -0600

    Added URLPattern to redirect from root to QTSurvey app

--- QTitan/QTitan/urls.py
+++ QTitan/QTitan/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^QTSurvey/', include('QTSurvey.urls')),
+    url(r'', include('QTSurvey.urls')),
     url(r'^admin/', admin.site.urls),
 ]
