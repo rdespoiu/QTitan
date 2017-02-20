905bcc294842d739eb75f9aaa1bf9198104a9755
rdespoiu <roberto.despoiu@gmail.com>
2017-02-20 08:48:41 -0600

    Researcher Subjects View Controller

--- QTitan/QTSurvey/Controllers/__init__.py
+++ QTitan/QTSurvey/Controllers/__init__.py
@@ -1,3 +1,4 @@
 from .ResearcherSurveys import getResearcherSurveys
+from .ResearcherSubjects import getResearcherSubjects
 from .SubjectAvailableSurveys import getSubjectAvailableSurveys
 from .SubjectCompletedSurveys import getSubjectCompletedSurveys
