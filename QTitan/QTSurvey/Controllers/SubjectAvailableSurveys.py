6e4ae6cb51902a3188f3dc671cd57cd8f0d826ff
rdespoiu <roberto.despoiu@gmail.com>
2017-02-20 12:06:59 -0600

    SubjectAvailableSurveys query needed a paren wrap to not include completed surveys

--- QTitan/QTSurvey/Controllers/SubjectAvailableSurveys.py
+++ QTitan/QTSurvey/Controllers/SubjectAvailableSurveys.py
@@ -3,8 +3,8 @@ from ..models import *
 def getSubjectAvailableSurveys(request):
     query = '''SELECT * FROM QTSurvey_Survey
                WHERE
-                 distribution = 0 or
-                 id in (SELECT SurveyID_ID FROM QTSurvey_SurveyAccess WHERE UserID_ID = {}) and
+                 (distribution = 0 or
+                 id in (SELECT SurveyID_ID FROM QTSurvey_SurveyAccess WHERE UserID_ID = {})) and
                  id not in (SELECT distinct SurveyID_ID from QTSurvey_CompletedSurvey WHERE UserID_ID = {})
                  '''.format(request.user.id, request.user.id)
 
