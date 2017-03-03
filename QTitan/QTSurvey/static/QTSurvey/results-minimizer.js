292616654418ae555cf7696c32ce650991d0af86
rdespoiu <roberto.despoiu@gmail.com>
2017-03-03 13:18:40 -0600

    Survey results minimizer complete

--- QTitan/QTSurvey/static/QTSurvey/results-minimizer.js
+++ QTitan/QTSurvey/static/QTSurvey/results-minimizer.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
   $('.minimizer').click(function() {
+    // Get participant ID
     let participantID = this.getAttribute('participantid');
-    let participantResults = document.getElementById(`participant${participantID}`);
 
-    participantResults.hide();
+    // Get div for current participant's results
+    let participantResults = $(`#participant${participantID}`);
+
+    // slideUp/slideDown toggle
+    participantResults.is(':visible') ? participantResults.slideUp('fast') : participantResults.slideDown('fast');
+
+    // Change button class
+    $(this).attr({'class': $(this).attr('class') == 'minimizer btn btn-danger' ? 'minimizer btn btn-primary' : 'minimizer btn btn-danger'});
+
+    // Set button text
+    this.innerHTML = this.innerHTML == '-' ? '+' : '-';
   });
 });
