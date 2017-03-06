450a6f6798a6a5db4291757853f9ec6b873e2a06
blazejkocon <bkocon95@gmail.com>
2017-03-05 23:28:51 -0600

    Fixed csrf(delete survey) issue

--- QTitan/QTSurvey/Controllers/DeleteSurvey.py
+++ QTitan/QTSurvey/Controllers/DeleteSurvey.py
@@ -5,5 +5,4 @@ from ..models import Survey
 class SurveyDelete(DeleteView):
     model = Survey
     success_url = reverse_lazy('index')
-    def get(self, request, *args, **kwargs):
-        return self.post(request, *args, **kwargs)
+    
