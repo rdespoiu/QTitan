a0f9d359eb35a06545360b8521c5acef727fb45e
Jacob Burkamper <jacob.burkamper@gmail.com>
2017-03-10 10:48:37 -0600

    Kmeans of individual responses, total weight calculation

--- QTitan/QTSurvey/Controllers/IdentifyClusters.py
+++ QTitan/QTSurvey/Controllers/IdentifyClusters.py
@@ -14,19 +14,43 @@ def identifyClusters(survey):
 	firstInterval = careSize
 
 	indices = {}
+	values = {}
 	index = 0
 	for field in fields:
 		indices[field.value] = index
+		values[index] = field.value
 		index += 1
+	
 	
 	print('{} {} {}'.format(str(lastInterval), str(midInterval), str(firstInterval)))
 	surveyResults = getAnalyticsData(survey)
-	responseMatrix = np.empty([2,1], dtype=int)
+	
+	#so hacky, but a better way to do this eludes me, so this is how it is.
+	total = 0
+	for participant in surveyResults:
+		total += len(surveyResults[participant])
+	responseMatrix = np.empty([total,2], dtype=int)
+	idx = 0
 	for participant in surveyResults:
 		for response in surveyResults[participant]:
-			responseMatrix = np.insert(responseMatrix, [response.orderPosition, indices[response.surveyFieldID.value]])
+			responseMatrix[idx] = [response.orderPosition, indices[response.surveyFieldID.value]]
+			idx += 1
 			print('[{},{}]'.format(str(response.orderPosition), str(indices[response.surveyFieldID.value])))
 	
-	responseMatrix
 	kmeans = KMeans(n_clusters=2, random_state=0).fit(responseMatrix)
-	return responseMatrix
+	
+	#calculate total reponse weight for each question
+	weights = {}
+	for participant in surveyResults:
+		for response in surveyResults[participant]:
+			split = response.orderPosition
+			if split < firstInterval or split > midInterval:
+				score = 3
+			else:
+				score = 0
+			
+			if response.surveyFieldID.value in weights:
+				weights[response.surveyFieldID.value] += score
+			else:
+				weights[response.surveyFieldID.value] = score
+	return weights
