ada37f16a9857959e7c3020ea839266913831688
Jacob Burkamper <jacob.burkamper@gmail.com>
2017-03-11 05:20:38 -0600

    Change gears, begin creating graph structure for identifying clusters

--- QTitan/QTSurvey/Controllers/IdentifyClusters.py
+++ QTitan/QTSurvey/Controllers/IdentifyClusters.py
@@ -4,6 +4,86 @@ import math
 from .GetAnalyticsData import getAnalyticsData
 from .GetSurveyFields import getSurveyFields
 
+
+############# RelationGraph class ###########
+class RelationGraph:
+	def __init__(self, surveyResults, numOptions):
+		self.surveyResults = surveyResults
+		self.lowerInterval = 3
+		self.midInterval = numOptions - 3
+
+		#generate a node for each participant and their results
+		self.Nodes = []
+		for participant in surveyResults:
+			self.Nodes.append(Node(participant, surveyResults[participant]))
+
+		#analyze each node for relationships
+		for node in self.Nodes:
+			self.Connect(node)
+	
+	def Connect(self, node):
+		#for all of this node's responses
+		for response in node.responses:
+			score = self.getScore(response.orderPosition)
+			# connect to the other nodes in this graph
+			for otherNode in self.Nodes:
+				if node == otherNode:
+					continue
+				
+				#find the score for this response option in the other node
+				for otherResponse in otherNode.responses:
+					if otherResponse.surveyFieldID.value == response.surveyFieldID.value:
+						otherScore = self.getScore(otherResponse.orderPosition)
+						break
+				
+				#if the intervals are the same
+				if score == otherScore:
+					node.addConnection(otherNode, score)
+					otherNode.addConnection(node, score)
+	
+	def getScore(self, pos):
+		ret = 0
+		if pos <= self.lowerInterval:
+			ret = 3
+		elif pos > self.lowerInterval and pos <= self.midInterval:
+			ret = 0
+		elif pos > self.midInterval:
+			ret = -3
+		return ret
+
+########## Node Class ##########################
+class Node:
+	def __init__(self, participant, responses):
+		self.connections = {}
+		self.responses = responses
+		self.participant = participant
+
+	#Adds a connection between this node and the incoming node.
+	def addConnection(self, node, magnitude):
+		key = node.participant.username
+		if key in self.connections:
+			self.connections[key] += magnitude
+		else:
+			self.connections[key] = magnitude
+
+
+
+########## Other functions #############
+def getOptionScore(survey, pos):
+	fields = getSurveyFields(survey)
+	numOptions = len(fields)
+	careSize = math.floor(numOptions / 10) + 1
+	careSize = careSize * 3
+	lastInterval = numOptions
+	midInterval = lastInterval - careSize
+	firstInterval = careSize
+	
+	if pos <= firstInterval or pos > midInterval:
+		score = 3
+	else:
+		score = 0
+	return score
+
 def identifyClusters(survey):
 	fields = getSurveyFields(survey)
 	numOptions = len(fields)
@@ -15,7 +95,7 @@ def identifyClusters(survey):
 
 	indices = {}
 	values = {}
-	index = 0
+	index = 1
 	for field in fields:
 		indices[field.value] = index
 		values[index] = field.value
@@ -27,30 +107,33 @@ def identifyClusters(survey):
 	
 	#so hacky, but a better way to do this eludes me, so this is how it is.
 	total = 0
+	index = 1
+	pindices = {}
 	for participant in surveyResults:
 		total += len(surveyResults[participant])
-	responseMatrix = np.empty([total,2], dtype=int)
+		pindices[participant.username] = index
+		index+=1
+	ptotal = len(surveyResults)
+	'''
+	numentries = (numOptions - (numOptions - 6)) * ptotal
+	responseMatrix = np.zeros([numentries+1,3], dtype=int)
 	idx = 0
-	for participant in surveyResults:
-		for response in surveyResults[participant]:
-			responseMatrix[idx] = [response.orderPosition, indices[response.surveyFieldID.value]]
-			idx += 1
-			print('[{},{}]'.format(str(response.orderPosition), str(indices[response.surveyFieldID.value])))
-	
-	kmeans = KMeans(n_clusters=2, random_state=0).fit(responseMatrix)
-	
-	#calculate total reponse weight for each question
 	weights = {}
 	for participant in surveyResults:
 		for response in surveyResults[participant]:
-			split = response.orderPosition
-			if split < firstInterval or split > midInterval:
-				score = 3
-			else:
-				score = 0
-			
+			score = getOptionScore(survey, response.orderPosition)
+			if score != 0:
+				responseMatrix[idx] = [pindices[participant.username], response.orderPosition, indices[response.surveyFieldID.value]]
+			idx += 1
 			if response.surveyFieldID.value in weights:
 				weights[response.surveyFieldID.value] += score
 			else:
 				weights[response.surveyFieldID.value] = score
-	return weights
+			print('[{},{}]'.format(str(response.orderPosition), str(indices[response.surveyFieldID.value])))
+	
+	kmeans = KMeans(n_clusters=2, random_state=0).fit(responseMatrix)
+	'''
+	fields = getSurveyFields(survey)
+	numOptions = len(fields)
+	G = RelationGraph(surveyResults, numOptions)
+	return G
