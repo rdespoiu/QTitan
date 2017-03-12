179474a9855744d8f69d1883d694264d492a8ae8
Jacob Burkamper <jacob.burkamper@gmail.com>
2017-03-11 19:31:39 -0600

    Code for identifying relational clusters in the RelationGraph

--- QTitan/QTSurvey/Controllers/IdentifyClusters.py
+++ QTitan/QTSurvey/Controllers/IdentifyClusters.py
@@ -1,6 +1,8 @@
 from sklearn.cluster import KMeans
 import numpy as np
 import math
+import random
+import copy
 from .GetAnalyticsData import getAnalyticsData
 from .GetSurveyFields import getSurveyFields
 
@@ -21,6 +23,9 @@ class RelationGraph:
 		for node in self.Nodes:
 			self.Connect(node)
 	
+	#Connect the node to the graph. The more strongly a node agrees on a response, the stronger the connection.
+	# The sum of these agreements determines the overall connection of the node.
+	# Therefore, nodes which have more important questions in common will be more strongly connected.
 	def Connect(self, node):
 		#for all of this node's responses
 		for response in node.responses:
@@ -41,6 +46,7 @@ class RelationGraph:
 					node.addConnection(otherNode, score)
 					otherNode.addConnection(node, score)
 	
+	#Identify the score of a any response at the given position
 	def getScore(self, pos):
 		ret = 0
 		if pos <= self.lowerInterval:
@@ -50,6 +56,88 @@ class RelationGraph:
 		elif pos > self.midInterval:
 			ret = -3
 		return ret
+	
+	#create and return a dictionary containing the total consensus, a sum of the scores for this question of all participants/nodes
+	def getTotalConsensus(self):
+		weights = {}
+		for node in self.Nodes:
+			for response in node.responses:
+				if response.surveyFieldID.value in weights:
+					weights[response.surveyFieldID.value] += getScore(response.orderPosition)
+				else:
+					weights[response.surveyFieldID.value] = getScore(response.orderPosition)
+		return weights
+	
+	def getClusters(self):
+		self.clusters = []
+		maxScore = self.getScore(0) # the score of the first option will always be the highest
+
+		#identify the range of minimum and maximum connection weights for a random node.
+		rn = random.choice(self.Nodes)
+		minCon, maxCon = rn.getConnectionRange()
+		
+		#Add each of the nodes in the graph to a cluster, creating clusters as appropriate
+		currentWeight = maxCon
+		clusteredNodes = []
+		cnum = 0
+		pool = []
+		for n in self.Nodes:
+			pool.append(n)
+		
+		### DEBUG
+			
+		
+		node = rn # start with the random node from above
+		while pool:
+			#If we have tried all the possible weights for this node
+			if currentWeight < minCon:
+				print("Iterating node")
+				node = random.choice(pool) # pick a random new node from those remaining
+				currentWeight = maxCon # reset the weight we are looking for
+
+			if currentWeight in node.connections.values():
+				c = Cluster(cnum, currentWeight, node)
+				cnum += 1
+				print(c)
+				maxWeight = currentWeight + maxScore
+				minWeight = currentWeight - maxScore
+				
+				# for each connected node
+				for n in node.connections:
+					#attach the node to the cluster if the connection strength is within the range of currentweight give or take the max score for a single option
+					# that is, attach the node to the cluster if the node agrees to the strength of this cluster, give or take a response
+					if n in pool:
+						self.assignToCluster(maxWeight, minWeight, pool, c, n, node)
+					
+				#add the created cluster to the list
+				self.clusters.append(c)
+
+			# if we didn't find a connection in this node at the current weight
+			print("iterating weight")
+			currentWeight -= (maxScore * 2) # decrement the weight we are looking for by 2 questions
+			
+			
+		return self.clusters
+			
+	#recursively add this 'node' to 'cluster' if the connection between it and its 'parent' is between maxWeight and minWeight, inclusive
+	# because it is recursive, it'll also add similar relationships this node has with other nodes it is connected to.
+	def assignToCluster(self, maxWeight, minWeight, pool, cluster, node, parent):
+		if parent != node and parent.connections[node] >= minWeight and parent.connections[node] <= maxWeight:
+			print("adding node {}, {} to cluster {}".format(node.participant.username, node, cluster))
+			if node in pool:
+				print("node in pool!")
+			else:
+				print("Node not in pool?")
+
+			added = cluster.addNode(node)
+			#if node in pool:
+			pool.remove(node)
+			
+			if added:
+				for n in node.connections:
+					if n in pool:
+						self.assignToCluster(maxWeight, minWeight, pool, cluster, n, node)
+		
 
 ########## Node Class ##########################
 class Node:
@@ -60,11 +148,33 @@ class Node:
 
 	#Adds a connection between this node and the incoming node.
 	def addConnection(self, node, magnitude):
-		key = node.participant.username
+		key = node
 		if key in self.connections:
 			self.connections[key] += magnitude
 		else:
 			self.connections[key] = magnitude
+
+	def getConnectionRange(self):
+		maxCon = max(self.connections.values())
+		minCon = min(self.connections.values())
+		return minCon, maxCon
+
+class Cluster:
+	def __init__(self, identifier, initialWeight, initialNode):
+		self.identifier = identifier
+		self.Nodes = []
+		self.Nodes.append(initialNode)
+		self.startWeight = initialWeight
+
+	def __str__(self):
+		return "Cluster" + str(self.identifier)
+
+	def addNode(self, node):
+		if not node in self.Nodes:
+			self.Nodes.append(node)
+			return True
+		else:
+			return False
 
 
 
